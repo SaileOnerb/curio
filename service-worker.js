f015d8b31fd414b068262099e892eead6eb93f87
SaileOnerb <165528518+SaileOnerb@users.noreply.github.com>
2026-09-24 22:11:26 -0300

    Refresh PWA cache for CURIÓ 2.7.1 branding

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='curio-2.7.0';
+const CACHE='curio-2.7.1';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
