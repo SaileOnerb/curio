60745686554d47b1b00a67320863afd12fc8e76b
SaileOnerb <165528518+SaileOnerb@users.noreply.github.com>
2026-09-24 22:34:09 -0300

    Support in-app updates for CURIÓ 2.7.3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='curio-2.7.2';
+const CACHE='curio-2.7.3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
@@ -10,3 +10,4 @@ self.addEventListener('fetch',e=>{
   }
   e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r})));
 });
+self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting()});
